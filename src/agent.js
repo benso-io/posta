c8a2b2a58b1bd34fe049c1637c643d961a5c3ea5
chen <chen@enso.security>
2021-01-27 13:08:02 +0200

    send to ture context content script

--- src/agent.js
+++ src/agent.js
@@ -1,36 +1,17 @@
 const uuid = require('uuid').v4;
-const agentFunction = require("./agent-body");
+const agentFunction = require("./agent-body").toString();
 
-const getFunctionBody = (func) => {
-    const string = func.toString()
-    return string.substring(
-        string.indexOf("{") + 1,
-        string.lastIndexOf("}")
-    )
-}
 
-let id=uuid();
-window.windowId=id;
+
+let windowId=uuid();
+window.windowId=windowId;
 var script = document.createElement('script');
-script.textContent = `window.windowId="${id}"
-${getFunctionBody(agentFunction)}`;
+script.textContent = `window.windowId="${windowId}";(${agentFunction})()`;
 document.documentElement.prepend(script);
 
-var port;
-const sendToBackground = (data)=>{
-    if (!port) {
-        try {
-            port = chrome.runtime.connect();    
-            port.onDisconnect.addListener(()=>port=undefined);
-        } catch (error) {
-            port = null;
-            return setTimeout(()=>{
-                sendToBackground(data);
-            },50)
-        }
-    }
-    port.postMessage(data);
-}
+chrome.runtime.onMessage.addListener((message, sender)=>{
+    var event = new CustomEvent("posta-relay", { detail: message });
+    window.dispatchEvent(event);
+})
 
-
-window.addEventListener("posta-telemetry", (event)=>event.detail? sendToBackground(event.detail):null);
+window.addEventListener("posta-telemetry", event=>chrome.runtime.sendMessage(event.detail))
